eb905b498d5894d9432aedd5ac8394de793a7fa1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-10-23 17:30:12 +0100

    Update install.py

--- install.py
+++ install.py
@@ -171,7 +171,7 @@ if not os.path.exists("/home/pi/.config/rclone/rclone.conf"):
         "expect \"Storage>\"",
         "send \"drive\\r\"",
         "expect \"client_id>\"",
-        "expect_user -re \"(.*)\\n\"",
+        "expect_user -timeout 3600 -re \"(.*)\\n\"",
         "send \"$expect_out(1,string)\\r\"",
         #"send \"556680234914-khamoi3j7tf3d723pe3n9u5ipvnlbsq5.apps.googleusercontent.com\\r\"",
         "expect \"client_secret>\"",
