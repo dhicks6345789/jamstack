fd49cd53dc1384d6b88e1df1d02a83c9a6ee0743
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-10-23 16:31:16 +0100

    Update install.py

--- install.py
+++ install.py
@@ -118,7 +118,7 @@ elif userSelection == 2:
     print("Code goes here...")
 elif userSelection == 3:
     copyfile("000-default-without-ssl.conf", "/etc/apache2/sites-available/000-default.conf", mode="0744")
-replaceVariables("/etc/apache2/sites-available/000-default.conf", {"DOMAINNAME":userOptions["-domianName"]})
+replaceVariables("/etc/apache2/sites-available/000-default.conf", {"DOMAINNAME":userOptions["-domainName"]})
 
 # Stop Apache while we update the config.
 os.system("apachectl stop")
