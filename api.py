0f9d1a6e1c9b8d06a285eeab00063cd5a0f513e2
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-10-24 14:20:25 +0100

    Update api.py

--- api.py
+++ api.py
@@ -2,10 +2,14 @@
 import flask
 
 app = flask.Flask(__name__)
-@app.route("/")
 
-def hello():
+@app.route("/")
+def api():
     return "Hello world!"
+
+@app.route("/build")
+def build():
+    return "Build..."
 
 if __name__ == "__main__":
     app.run()
