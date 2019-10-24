070a669307b553e3425ec465f3e09c9d3b88f7fb
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-10-24 16:53:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sleep 10
-python3 /usr/local/bin/docsToMarkdown.py -c /var/local/docsToMarkdown.json -i /mnt/content -o /var/local/jekyll -t /mnt/jekyll > /var/log/docsToMarkdown.log 2>&1
-
-#export LC_ALL="en_US.UTF-8"
-#export LANG="en_US.UTF-8"
-#cd /var/local/accessDisputesCommittee/jekyll; bundle exec jekyll build --destination /var/www/html --incremental >> /var/local/accessDisputesCommittee/log.txt 2>&1; cd
-#/usr/local/bin/tidyHTML.py /var/www/html > /var/local/accessDisputesCommittee/log.txt 2>&1
+python3 /usr/local/bin/docsToMarkdown.py -c /var/local/docsToMarkdown.json -i /mnt/content -o /var/local/jekyll -t /mnt/jekyll > /var/log/build.log 2>&1
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+cd /var/local/jekyll; bundle exec jekyll build --destination /var/www/html --incremental >> /var/log/build.log 2>&1; cd
+/usr/local/bin/tidyHTML.py /var/www/html >> /var/log/build.log 2>&1
