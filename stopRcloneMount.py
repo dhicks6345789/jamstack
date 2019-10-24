fd93d949e1f1fa600bbf55d25f3478d7c6ca52f4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-10-24 13:03:50 +0100

    Delete stopRcloneMount.py

--- stopRcloneMount.py
+++
@@ -1,6 +0,0 @@
-psHandle = os.popen("ps ax")
-for psLine in psHandle.readlines():
-    if "rclone mount" in psLine:
-        print("Unmounting content repository...")
-        os.system("kill " + psLine.split()[0])
-psHandle.close()
