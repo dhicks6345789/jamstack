43f373336b853fa95e15afeb33ba2f3ba02c5353
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-10-30 11:55:07 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-#python3 /usr/local/bin/docsToMarkdown.py -c /var/local/docsToMarkdown.json -i /mnt/content -o /var/local/jekyll -t /mnt/jekyll > /var/log/build.log 2>&1
+startTime=$SECONDS
+echo "Start time: $startTime" > /var/log/build.log
+#python3 /usr/local/bin/docsToMarkdown.py -c /var/local/docsToMarkdown.json -i /mnt/content -o /var/local/jekyll -t /mnt/jekyll >> /var/log/build.log 2>&1
+docsToMarkdownRuntime=$(( SECONDS - startTime ))
+echo "DocsToMarkdown run time: $docsToMarkdownRuntime" >> /var/log/build.log
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 cd /var/local/jekyll; bundle exec jekyll build --destination /var/www/html --incremental >> /var/log/build.log 2>&1; cd
+jekyllRuntime=$(( SECONDS - docsToMarkdownRuntime ))
+echo "Jekyll run time: $jekyllRuntime" >> /var/log/build.log
 /usr/local/bin/tidyHTML.py /var/www/html >> /var/log/build.log 2>&1
+tidyRuntime=$(( SECONDS - jekyllRuntime ))
+echo "Tidy run time: $tidyRuntime" >> /var/log/build.log
