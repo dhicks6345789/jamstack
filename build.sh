0c60daa9260ad7912b1fcb81cd4f5d47bb5cd8b5
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-10-30 11:48:08 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 /usr/local/bin/docsToMarkdown.py -c /var/local/docsToMarkdown.json -i /mnt/content -o /var/local/jekyll -t /mnt/jekyll > /var/log/build.log 2>&1
+#python3 /usr/local/bin/docsToMarkdown.py -c /var/local/docsToMarkdown.json -i /mnt/content -o /var/local/jekyll -t /mnt/jekyll > /var/log/build.log 2>&1
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 cd /var/local/jekyll; bundle exec jekyll build --destination /var/www/html --incremental >> /var/log/build.log 2>&1; cd
