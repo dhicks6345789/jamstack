d9f66539406d6a4466a3324dae97a9f57de6730b
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-10-30 18:06:17 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 startTime=$SECONDS
-echo "Start time: $startTime" > /var/log/build.log
+echo "Starting..." > /var/log/build.log
 python3 /usr/local/bin/docsToMarkdown.py -c /var/local/docsToMarkdown.json -i /mnt/content -o /var/local/jekyll -t /mnt/jekyll >> /var/log/build.log 2>&1
 docsToMarkdownRuntime=$(( SECONDS - startTime ))
-echo "DocsToMarkdown run time: $docsToMarkdownRuntime" >> /var/log/build.log
+echo "DocsToMarkdown run time: $docsToMarkdownRuntime seconds." >> /var/log/build.log
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 cd /var/local/jekyll; bundle exec jekyll build --destination /var/www/html --incremental >> /var/log/build.log 2>&1; cd
 jekyllRuntime=$(( SECONDS - docsToMarkdownRuntime ))
-echo "Jekyll run time: $jekyllRuntime" >> /var/log/build.log
+echo "Jekyll run time: $jekyllRuntime seconds." >> /var/log/build.log
 /usr/local/bin/tidyHTML.py /var/www/html >> /var/log/build.log 2>&1
 tidyRuntime=$(( SECONDS - jekyllRuntime ))
 totalRuntime=$(( tidyRuntime - startTime ))
-echo "Tidy run time: $tidyRuntime" >> /var/log/build.log
+echo "Tidy run time: $tidyRuntime seconds." >> /var/log/build.log
+echo "Done! Total run time: $totalRuntime seconds." >> /var/log/build.log
